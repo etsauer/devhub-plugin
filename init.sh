95988bf5699a52211c56470886705fb7cb1d0675
cgruver <cgruver@clg.lab>
2024-07-01 18:02:28 +0000

    add env initialization to yarn dev

--- init.sh
+++ init.sh
@@ -1,7 +1,3 @@
-#!/usr/bin/env bash
-
 export FRONTEND_URL=https://$(oc get route ${DEVWORKSPACE_ID}-dev-tools-3000-frontend -o jsonpath={.spec.host})
 export INSECURE_FRONTEND_URL=http://$(oc get route ${DEVWORKSPACE_ID}-dev-tools-3000-frontend -o jsonpath={.spec.host})
 export BACKEND_URL=https://$(oc get route ${DEVWORKSPACE_ID}-dev-tools-7007-backend -o jsonpath={.spec.host})
-# export DANGEROUSLY_DISABLE_HOST_CHECK=true
-yarn dev
